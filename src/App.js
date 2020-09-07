90ac61732b2126b9679bd7a8cc27b89bc45895d9
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-07 15:21:52 +0100

    All buttons correctly updating state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ({
+  player1,
+  player2,
+  handleIncrementP1,
+  handleIncrementP2,
+  handleReset,
+}) => (
+  <React.Fragment>
+    {/* header */}
+    <header className="jumbotron mt-4 mb-0">
+      <h1>PongPing</h1>
+    </header>
+
+    {/* scores */}
+    <div className="row mb-4">
+      <div className="col-md-6 mt-4">
+        <div className="card text-center bg-dark text-white">
+          <h5 className="card-header">Player 1</h5>
+          <div className="card-body">
+            <p className="card-text display-1">{player1}</p>
+          </div>
+          <div className="card-footer">
+            <button
+              className="form-control btn btn-success"
+              onClick={handleIncrementP1}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-6 mt-4">
+        <div className="card text-center">
+          <h5 className="card-header">Player 2</h5>
+          <div className="card-body">
+            <p className="card-text display-1">{player2}</p>
+          </div>
+          <div className="card-footer">
+            <button
+              className="form-control btn btn-success"
+              onClick={handleIncrementP2}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    {/* winner message */}
+    <h2 className="alert alert-success">
+      Player {/* winning player here */} wins!
+    </h2>
+
+    <hr />
+
+    {/* reset button */}
+    <button className="btn btn-danger" onClick={handleReset}>
+      Reset
+    </button>
+  </React.Fragment>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENTP1":
+      return { ...state, player1: state.player1 + 1 };
+    case "INCREMENTP2":
+      return { ...state, player2: state.player2 + 1 };
+    case "RESET":
+      return initial;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  reducer,
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const render = () => {
+  let state = store.getState();
+  ReactDOM.render(
+    <App
+      player1={state.player1}
+      player2={state.player2}
+      handleIncrementP1={() => store.dispatch({ type: "INCREMENTP1" })}
+      handleIncrementP2={() => store.dispatch({ type: "INCREMENTP2" })}
+      handleReset={() => store.dispatch({ type: "RESET" })}
+    />,
+    document.getElementById("root")
+  );
+};
+
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
